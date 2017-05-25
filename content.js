4ba3779c24ad192f9844f6a92e465589191993a0
samstephenson <sam.kayaker@hotmail.com>
2017-05-25 13:16:36 -0700

    0.1.2 Starred up top

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 
-	console.log("JS working");
+	console.log("JS workingg");
 
 	$('#buttonBar').insertAfter($('.showCompletedButton'));
 	$( "<div style='flex-grow: 1;'></div>").insertAfter( "#searchForm" );
 	$('#savedViewHUDButton').insertAfter($('#searchForm'));
+
+	$('.content:empty').addClass('empty');
+
+
 
 });
