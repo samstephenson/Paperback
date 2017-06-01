cce17355309c326ff2a50f7bcd437b8bfaab2677
samstephenson <sam.kayaker@hotmail.com>
2017-05-31 22:07:10 -0700

    new header first pass
    Icons in place, menus re-shuffled. Still lots to smooth out

--- content.js
+++ content.js
@@ -3,11 +3,27 @@ $(document).ready(function() {
 	console.log("JS workingg");
 
 	$('#buttonBar').insertAfter($('.showCompletedButton'));
-	$( "<div style='flex-grow: 1;'></div>").insertAfter( "#searchForm" );
-	$('#savedViewHUDButton').insertAfter($('#searchForm'));
+	$('#savedViewHUDButton').insertAfter($('#logo'));
+	$('.showCompletedButton').insertBefore($('#settingsButton'));
+	$( "<div style='flex-grow: 1;'></div>").insertAfter( "#savedViewHUDButton" );
+	$('#helpButton').insertAfter($('#savedViewHUD'));
+	$('.saveButton').insertBefore($('#searchForm'));
+
+	$('#searchBox').removeAttr('placeholder');
+
 
 	$('.content:empty').addClass('empty');
 
+	$(window).on('scroll', function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = 10;             // set to whatever you want it to be
 
+    if(y_scroll_pos > scroll_pos_test) {
+        //do stuff
+        $('#header').addClass("scroll");
+    } else {
+    	$('#header').removeClass("scroll");
+    }
+});
 
 });
